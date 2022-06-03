a8b980568ac18e137fa7595478b7d23e09ff7851
FuBooo <baihe2233@gmail.com>
2022-06-03 22:35:52 +0800

    fix: request fail to error pages

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,12 +1,49 @@
 import Axios, { AxiosInstance } from 'axios';
 import { createContext, useContext } from 'react';
 
+declare module 'axios' {
+  export interface AxiosRequestConfig {
+    retry?: number;
+    retryDelay?: number;
+  }
+}
+
 const axios = Axios.create({
   baseURL: '/api',
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
+  retry: 5,
+  retryDelay: 1000,
+});
+
+axios.interceptors.response.use(undefined, (error) => {
+  const config = error.config;
+  // skip retry for failed requests from the cache
+  if (!config || config.retry) return Promise.reject(error);
+
+  // set retry count
+  config.__retryCount = config.__retryCount || 0;
+
+  // over retry limit
+  if (config.__retryCount >= config.retry) {
+    // return error and auto retry
+    return Promise.reject(error);
+  }
+
+  config.__retryCount += 1;
+
+  const backoff = new Promise<void>((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, config.retryDelay || 1000);
+  });
+
+  // return retry
+  return backoff.then(function () {
+    return axios(config);
+  });
 });
 
 axios.interceptors.response.use(
