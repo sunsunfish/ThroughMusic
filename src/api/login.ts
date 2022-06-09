69274c7c58ddceecc3d21692eb3b6d48807914fa
ouliyong <916879561@qq.com>
2022-06-09 11:07:33 +0800

    test: 让小天才看问题

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,8 +2,8 @@ import { useRequest } from './request';
 import { IUserAccount } from './types';
 
 // 手机登录
-const useLoginCellphone = (phone: string, password: string) => {
-  return useRequest<
+const useLoginCellphone = (phone: string, password: string) =>
+  useRequest<
     { phone: string; password: string },
     {
       token: string;
@@ -17,10 +17,9 @@ const useLoginCellphone = (phone: string, password: string) => {
       password,
     },
     key: `loginCellphone-${phone}`,
-    // options: {
-    //   enabled: false,
-    // },
+    options: {
+      enabled: false,
+    },
   });
-};
 
 export { useLoginCellphone };
