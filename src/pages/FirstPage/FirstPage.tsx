a117f21604effa4488f1ea3dd2e016a69e60c972
chenchilong <chilong.chen@shopcider.com>
2022-06-23 19:58:04 +0800

    轮播图

--- src/pages/FirstPage/FirstPage.tsx
+++ src/pages/FirstPage/FirstPage.tsx
@@ -1,37 +1,95 @@
 import Meta from '@/components/Meta';
-import { Content, ImageList, Lineb, Uone, Li } from './styled';
+import { useEffect, useState } from 'react';
+import {
+  SwipeContainer,
+  SwipeItem,
+  SwipeItemWrap,
+  SwipeDotWrap,
+  SwipeDotItem,
+  ArrowContainer,
+  Arrow,
+} from './styled';
 
 //轮播组件
 function FirstPage() {
+  const [index, setIndex] = useState(0);
+  const swipeList = [
+    'https://img0.baidu.com/it/u=530426417,2082848644&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
+    'https://img1.baidu.com/it/u=700675537,3936578503&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
+    'https://img0.baidu.com/it/u=3798217922,3880088897&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500',
+    'https://img1.baidu.com/it/u=1966616150,2146512490&fm=253&fmt=auto&app=138&f=JPEG?w=751&h=500',
+  ];
+
+  const getIndex = (pos: 'left' | 'right') => {
+    if (pos === 'left') {
+      return index - 1 < 0 ? swipeList.length - 1 : index - 1;
+    } else {
+      return index + 1 > swipeList.length - 1 ? 0 : index + 1;
+    }
+  };
+
+  const preSwipe = () => {
+    setIndex(getIndex('left'));
+  };
+
+  const nextSwipe = () => {
+    setIndex(getIndex('right'));
+  };
+
+  const mouseEnter = (idx: number) => {
+    setIndex(idx);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      nextSwipe();
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <>
       <Meta title="test" />
-      <Content>
-        <ImageList>
-          <Lineb>
-            <Uone>
-              <Li>
-                <img src=" http://www.runoob.com/images/pulpit.jpg"></img>{' '}
-              </Li>
-              <Li>
-                <img src=" http://www.runoob.com/images/pulpit.jpg"></img>
-              </Li>
-              <Li>
-                <img src=" http://www.runoob.com/images/pulpit.jpg"></img>
-              </Li>
-              <Li>
-                <img src=" http://www.runoob.com/images/pulpit.jpg"></img>
-              </Li>
-              <Li>
-                <img src=" http://www.runoob.com/images/pulpit.jpg"></img>
-              </Li>
-              <Li>
-                <img src=" http://www.runoob.com/images/pulpit.jpg"></img>
-              </Li>
-            </Uone>
-          </Lineb>
-        </ImageList>
-      </Content>
+      <div>
+        <SwipeContainer>
+          {
+            ['left','right'].map((item ,index)=> (
+             <ArrowContainer className= {item} onClick={item === 'right' ? preSwipe : nextSwipe} key = {index}>
+              <Arrow className = {item}></Arrow>
+            </ArrowContainer>
+            )
+          }
+          <SwipeItemWrap>
+            {swipeList.map((item, idx) => (
+              <SwipeItem
+                key={idx}
+                className={`${
+                  idx === index
+                    ? 'center'
+                    : getIndex('right') === idx
+                    ? 'right'
+                    : getIndex('left') === idx
+                    ? 'left'
+                    : ''
+                }`}
+              >
+                <img src={item} />
+              </SwipeItem>
+            ))}
+          </SwipeItemWrap>
+          <SwipeDotWrap>
+            {swipeList.map((_, idx) => (
+              <SwipeDotItem
+                onMouseEnter={() => mouseEnter(idx)}
+                className={`${index === idx ? 'active' : ''}`}
+                key={idx}
+              />
+            ))}
+          </SwipeDotWrap>
+        </SwipeContainer>
+      </div>
     </>
   );
 }
--- src/pages/FirstPage/styled.ts
+++ src/pages/FirstPage/styled.ts
@@ -1,46 +1,119 @@
 import { styled } from '@mui/system';
 
-const Content = styled('div')({
-  width: '1200px',
-  height: '367px',
-  margin: '100px auto 0',
-  backgroundColor: 'red',
+//整个盒子
+const SwipeContainer = styled('div')({
+  backgroundColor: 'skyblue',
+  width: '700px',
+  height: '350px',
+  overflow: 'hidden',
+  position: 'relative',
+  margin: '0 auto',
 });
 
-const ImageList = styled('div')({
-  width: '1200px',
-  height: '336px',
-  backgroundColor: '#2758d8',
-  margin: '0',
-  // 'ImageList>u1':{
-  //   width: '100%',
-  //   height: '100%',
-  // },
-  // 'ImageList>u1>li':{
-  //   position:"absolute",
-  //   width: '730px',
-  //   height: '336px',
-  // }
-});
-
-const Lineb = styled('div')({
-  width: '1200px',
-  height: '31px',
-  backgroundColor: '#33b630',
+//包裹轮播列表的样式
+const SwipeItemWrap = styled('ul')({
+  display: 'flex',
+  padding: '40px 0',
   margin: '0',
 });
 
-const Uone = styled('u')({
-  paddingLeft: '0',
+//轮播图列表
+const SwipeItem = styled('li')({
+  backgroundColor: 'orange',
   listStyle: 'none',
-  margin: '0',
-  width: '100%',
-  height: '100%',
+  width: '300px',
+  height: '200px',
+  flexShrink: '0',
+  transition: 'all 0.5s ease-in-out',
+  position: 'absolute',
+  left: '28.5%',
+  zIndex: '-1',
+  img: {
+    width: '100%',
+    height: '100%',
+  },
+  '&.left': {
+    zIndex: '1',
+    transform: 'scale(1)translateX(-50%)',
+  },
+  '&.right': {
+    zIndex: '2',
+    transform: 'scale(1) translateX(50%)',
+  },
+  '&.center': {
+    zIndex: '3',
+    transform: 'scale(1.2) translateX(0)',
+  },
 });
 
-const Li = styled('li')({
+//包裹轮播图下面的点点标识
+const SwipeDotWrap = styled('ul')({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  columnGap: '20px',
   position: 'absolute',
-  width: '730px',
-  height: '336px',
+  left: '50%',
+  transform: 'translateX(-50%)',
+  bottom: 0,
 });
-export { Content, ImageList, Lineb, Uone, Li };
+
+//轮播图下面的点点标识
+const SwipeDotItem = styled('ul')(({ theme }) => ({
+  backgroundColor: '#e5e5e5',
+  width: '10px',
+  height: '10px',
+  borderRadius: '50%',
+  flexShrink: '0',
+  padding: '0',
+  '&.active': {
+    backgroundColor: theme.palette.primary.main,
+    transition: 'all 0.5s ease-in-out',
+  },
+}));
+
+//左右箭头按钮
+const ArrowContainer = styled('div')({
+  width: '40px',
+  height: '60px',
+  position: 'absolute',
+  backgroundColor: 'transparent',
+  top: '35%',
+  '&:hover': {
+    backgroundColor: '#c0c0c0',
+    background: 'rgba(0, 0, 0, .1)',
+  },
+  '&.left': {
+    left: '0',
+  },
+  '&.right': {
+    right: '0',
+  },
+});
+
+const Arrow = styled('div')({
+  position: 'relative',
+  top: '27%',
+  width: '20px',
+  height: '20px',
+  borderTop: '3px solid #fff',
+  borderRight: '3px solid #fff',
+  '&.left': {
+    left: '10px',
+    transform: 'rotate(-135deg)',
+  },
+  '&.right': {
+    right: '-10px',
+    transform: 'rotate(45deg)',
+  },
+});
+
+export {
+  SwipeContainer,
+  SwipeItemWrap,
+  SwipeItem,
+  SwipeDotWrap,
+  SwipeDotItem,
+  ArrowContainer,
+  Arrow,
+};
